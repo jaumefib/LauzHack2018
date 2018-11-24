1a601f15f7b56634464a1e9d3f8c64a2b1d858d8
jaumefib <jaume.pladevall@est.fib.upc>
2018-11-24 16:32:04 +0100

    Modif errors Python

--- read.py
+++ read.py
@@ -1,11 +1,12 @@
 actStation = 0
 
 def insertStation(station, lon, lat, peopleIn, peopleOut):
+    global actStation
     actStation = actStation + 1
     return actStation
 
-def insertWalkable(idIn, idOut)
-    return true
+def insertWalkable(idIn, idOut):
+    return True
 
 def insertLink(idIn, idOut, line, frequencies, people, kind, price):
-    return true
+    return True
