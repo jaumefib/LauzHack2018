d6cd464a39406dae6ea212972876dedf309361c2
oriolclosa <oriol.closa@est.fib.upc.edu>
2018-11-24 16:05:00 +0100

    Initial definition

---
+++ read.py
@@ -0,0 +1,11 @@
+actStation = 0
+
+def insertStation(station, lon, lat, peopleIn, peopleOut):
+    actStation = actStation + 1
+    return actStation
+
+def insertWalkable(idIn, idOut)
+    return true
+
+def insertLink(idIn, idOut, line, frequencies, people, kind, price):
+    return true
