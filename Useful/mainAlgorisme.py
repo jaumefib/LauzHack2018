f52d8918004c0a5c97a912af774518fd105dc89a
jaumefib <jaume.pladevall@est.fib.upc>
2018-11-25 00:08:51 +0100

    Algorism

---
+++ Useful/mainAlgorisme.py
@@ -0,0 +1,22 @@
+from utils import (
+    argmax_random_tie, probability
+)
+
+from Node import Node
+
+
+def hill_climbing(problem):
+    """From the initial node, keep choosing the neighbor with highest value,
+    stopping when no neighbor is better. [Figure 4.2]"""
+    current = Node(problem.initial)
+    while True:
+        neighbors = current.expand(problem)
+        if not neighbors:
+            break
+        neighbor = argmax_random_tie(neighbors,
+                                     key=lambda node: problem.value(node.state))
+        if problem.value(neighbor.state) <= problem.value(current.state):
+            break
+        current = neighbor
+    return current.state
+
