ffd60b5fc93b2d08dbcea5c2b541a718d279a91e
jaumefib <jaume.pladevall@est.fib.upc>
2018-11-25 05:38:40 +0100

    funcions beta

--- funcions.py
+++ funcions.py
@@ -1,25 +1,37 @@
 import random
 
 def actualGraphCost():
-    return 60
+    return random.randint(100, 120)
+
 
 def obtainLines():
-    return {1:"R1",2:"R2",3:"R3"}
+    return {1: "R1", 2: "R2", 3: "R3"}
+
 
 def obtainLinesMostUsed(lines):
-    return {2:"R2",1:"R1",3:"R3"}
+    return {2: "R2", 1: "R1", 3: "R3"}
 
-def modifyFreq(linia,accio):
-    if accio == "increment"
+
+def modifyFreq(linia, accio):
+    if accio == "increment":
+        r = 1
         # increment the frecuency of the line
         # only if the value is less than the maximum
     elif accio == "decrease":
+        r = 2
         # decrease the frecuency of the line
         # only if the value is greater than the minimum
 
+
 def calculateCost():
-    return 50
+    return random.randint(40, 60)
+
 
 def calculateGraph():
+    calcular = 100
     # For the last modifyFreq with the modification in the line
     # change the graph 
+
+def paintGraph():
+    # paint the resultant graph
+    print("Painting the graph")
--- special.py
+++ special.py
@@ -1,24 +1,29 @@
+from funcions import *
+
 infinity = float('inf')
 
-import funcions
 
 def hill_climbing(problem):
     """From the initial node, keep choosing the neighbor with highest value,
     stopping when no neighbor is better. [Figure 4.2]"""
 
     problem.cost = actualGraphCost(); # ToDo: David
-
+    print("Initial problem cost " + str(problem.cost))
     while True:
         lines = obtainLines() # ToDo: Oriol
         neighbours = obtainLinesMostUsed(lines) # ToDo: David
+        print("Linies en ordre de utilitzacio " + str(neighbours))
 
         b = True
 
         for i in neighbours:
-            modifyFreq(negibour[i],"increment") # ToDo: David
+            modifyFreq(neighbours[i], "increment") # ToDo: David
             cost = calculateCost() # ToDo: David
+            print(str(neighbours[i]) + " " + str(cost))
             if problem.cost > cost:
+                print("problem cost > cost")
                 problem.cost = cost
+                print("Nou cost minim " + str(problem.cost))
                 b = False
                 break
         # If there is no better state we finish
